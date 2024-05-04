4d14719ef66b4cc5c354f8e30beede87c0af8544
Pacho <paulpupo96@gmail.com>
2024-05-04 16:45:21 -0700

    Project 4: Screen fill and Multiplication assembly files. Project 4 zipped

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,4 +8,48 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(FILL)
+    @R0
+    M=0 // Init RAM[0] for counting
+    (FILL_LOOP)
+        @SCREEN
+        D=A
+        @R0
+        D=D+M
+        @R1
+        M=D // Set address RAM[1] = SCREEN + RAM[0]
+
+        @KBD
+        D=M
+        @BLACK
+        D;JNE
+        (WHITE)
+            @R1
+            A=M
+            M=0
+            @BLACK_END
+            0;JMP
+        (WHITE_END)
+        (BLACK)
+            @R1
+            A=M
+            M=-1
+        (BLACK_END)
+
+        @R0
+        M=M+1 // ++RAM[0]
+        
+        @R1
+        D=M
+        @KBD
+        D=A-D
+        @FILL_LOOP
+        D;JNE   // Repeat until we've reached KBD address space
+    (FILL_LOOP_END)
+    @FILL
+    0;JMP
+(FILL_END)
+
+(END)
+    @END
+    0;JMP
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@R2
+M=0 // Zero output RAM[2]
+
+@R0
+D=M
+@END
+D;JEQ // if(RAM[0] == 0), goto end
+
+@R1
+D=M
+@END
+D;JEQ // if(RAM[1] == 0), goto end
+
+@3  // Instantiate loop variable with RAM[1] into RAM[3]
+M=D
+(LOOP)
+    @3
+    D=M;
+    @LOOP_END
+    D;JEQ   // if(RAM[3] == 0), goto end
+
+    @0
+    D=M
+    @2
+    M=D+M   // Add RAM[0] to Output RAM[2]
+
+    @3
+    M=M-1   // --RAM[3]
+    @LOOP
+    0;JMP
+(LOOP_END)
+
+(END)
+    @END
+    0;JMP
