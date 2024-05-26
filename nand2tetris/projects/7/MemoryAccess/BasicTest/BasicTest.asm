9cbbafae9256ccce9df3a4018120d88d2e6f5b54
Pacho <paulpupo96@gmail.com>
2024-05-26 13:32:32 -0700

    Project 7: Implementation of vm2asm. Tested on BasicTest.vm to .asm.

---
+++ nand2tetris/projects/7/MemoryAccess/BasicTest/BasicTest.asm
@@ -0,0 +1,286 @@
+// Virtual Machine code to assembly code in C++ for the Hack computer.
+//		Sonaurea/RoyalArdor
+// Initializations
+// Stack Init
+@256
+D=A
+@SP
+M=D
+
+// Push constant 10
+@10
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Pop local 0
+@0
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Push constant 21
+@21
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Push constant 22
+@22
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Pop argument 2
+@2
+D=A
+@ARG
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Pop argument 1
+@1
+D=A
+@ARG
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Push constant 36
+@36
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Pop this 6
+@6
+D=A
+@THIS
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Push constant 42
+@42
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Push constant 45
+@45
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Pop that 5
+@5
+D=A
+@THAT
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Pop that 2
+@2
+D=A
+@THAT
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Push constant 510
+@510
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Pop temp 6
+@6
+D=A
+@5
+D=A+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// Push local 0
+@0
+D=A
+@LCL
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Push that 5
+@5
+D=A
+@THAT
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Add Op
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+M=M+D
+
+// Push argument 1
+@1
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Sub Op
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+M=M-D
+
+// Push this 6
+@6
+D=A
+@THIS
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Push this 6
+@6
+D=A
+@THIS
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Add Op
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+M=M+D
+
+// Sub Op
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+M=M-D
+
+// Push temp 6
+@6
+D=A
+@5
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// Add Op
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+M=M+D
+
+// End of program
+(END)
+@END
+0;JMP
---
+++ nand2tetris/projects/7/vm2asm.cpp
@@ -0,0 +1,230 @@
+#include <iostream> // io
+#include <fstream>  // files
+#include <sstream>  // string stream
+#include <string>   // string
+#include <format>   // format
+#include <stdint.h> // uint
+#include <vector>   // vector
+#include <unordered_map>// unordered_map
+using namespace std;
+
+static string mem_push(string segment, int idx);
+static string mem_pop(string segment, int idx);
+static string arith_add();
+static string arith_sub();
+static string arith_neg();
+static string arith_eq();
+static string arith_gt();
+static string arith_lt();
+static string arith_and();
+static string arith_or();
+static string arith_not();
+
+static const unordered_map<string, string(*)(string, int)> memAccessMap
+{
+    {"push", &mem_push}, {"pop", &mem_pop}
+};
+
+static const unordered_map<string, string> segmentMap
+{
+    {"local", "LCL"}, {"argument", "ARG"},
+    {"this", "THIS"}, {"that", "THAT"}, {"temp", "5"}
+};
+
+static string mem_push(string segment, int idx)
+{
+    string asmLine="";
+    if(segmentMap.contains(segment) || segment == "constant")
+    {
+        asmLine = format("// Push {} {}\n", segment, idx);
+        asmLine += format("@{}\nD=A\n", idx);
+        if(segmentMap.find(segment)!=segmentMap.end())
+        {
+            asmLine += format("@{}\n", segmentMap.at(segment));
+            asmLine += (segment == "temp") ? "A=A+D\n": "A=M+D\n"; // Temp is const, else indirection
+            asmLine += "D=M\n";
+        }
+        asmLine += "@SP\nA=M\nM=D\n@SP\nM=M+1\n"; // Add D to stack and increment sp
+    }
+    return asmLine;
+}
+
+static string mem_pop(string segment, int idx)
+{
+    string asmLine="";
+    if(segmentMap.find(segment)!=segmentMap.end())
+    {
+        asmLine = format("// Pop {} {}\n", segment, idx);
+        asmLine += format("@{}\nD=A\n@{}\n", idx, segmentMap.at(segment));
+        asmLine += (segment == "temp") ? "D=A+D\n": "D=M+D\n"; // Temp is const, else indirection
+        asmLine += "@R13\nM=D\n";
+        asmLine += "@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"; // Decrement SP and add popped value
+    }
+    return asmLine;
+}
+
+static const unordered_map<string, string(*)()> arithmeticMap
+{
+    {"add", &arith_add}, {"sub", &arith_sub}, {"neg", &arith_neg}, 
+    {"eq", &arith_eq}, {"gt", &arith_gt}, {"lt", &arith_lt}, 
+    {"and", &arith_and}, {"or", &arith_or}, {"not", &arith_not}
+};
+
+static string arith_add()
+{
+    string asmLine = "// Add Op\n";
+    asmLine += "@SP\nAM=M-1\nD=M\n"; // First value
+    asmLine += "@SP\nA=M-1\nM=M+D\n";  // Second value
+    return asmLine;
+}
+static string arith_sub()
+{
+    string asmLine = "// Sub Op\n";
+    asmLine += "@SP\nAM=M-1\nD=M\n"; // First value
+    asmLine += "@SP\nA=M-1\nM=M-D\n"; // Second value
+    return asmLine;
+}
+static string arith_neg()
+{
+    string asmLine = "// Neg Op\n";
+    asmLine += "@SP\nA=M-1\nM=-M\n";  // Negate First value
+    return asmLine;
+}
+static string arith_comp(string comp)
+{
+    static unordered_map<string, uint8_t> jumpCount;
+    if(!jumpCount.contains(comp)) jumpCount[comp] = 0;
+    string asmLine = format("// {} op\n", comp);
+    asmLine += "@SP\nAM=M-1\nD=M\n"; // First value
+    asmLine += "@SP\nAM=M-1\nD=M-D\n"; // Subtract second value
+    asmLine += format("@{}_TRUE_{}\nD;{}\n", comp, jumpCount[comp], comp); // add jump comparator
+    asmLine += "@SP\nA=M\nM=0\n"; // Push 0 (false)
+    asmLine += format("@{}_END_{}\n0;JMP\n", comp, jumpCount[comp]); // Jump end
+    asmLine += format("({}_TRUE_{})\n@SP\nA=M\nM=-1", comp, jumpCount[comp]); // Push -1 (true)
+    asmLine += format("({}_END_{})\n@SP\nM=M+1\n", comp, jumpCount[comp]); // Increment sp
+    ++jumpCount[comp];
+    return asmLine;
+}
+static string arith_eq()
+{
+    return arith_comp("JEQ");
+}
+static string arith_gt()
+{
+    return arith_comp("JGT");
+}
+static string arith_lt()
+{
+    return arith_comp("JLT");
+}
+static string arith_and()
+{
+    string asmLine = "// AND op\n";
+    asmLine += "@SP\nAM=M-1\nD=M\n"; // First value
+    asmLine += "@SP\nA=M-1\nM=D&M\n"; // And second value
+    asmLine += "@SP\nM=M+1\n"; // Increment sp
+    return asmLine;
+}
+static string arith_or()
+{
+    string asmLine = "// OR op\n";
+    asmLine += "@SP\nAM=M-1\nD=M\n"; // First value
+    asmLine += "@SP\nA=M-1\nM=D|M\n"; // Or second value
+    asmLine += "@SP\nM=M+1\n"; // Increment sp
+    return asmLine;
+}
+static string arith_not()
+{
+    string asmLine = "// Not op\n";
+    asmLine += "@SP\nAM=M-1\nM=!M\n"; // Not first value
+    return asmLine;
+}
+
+/*
+ * Splits a string into words
+ */
+static vector<string> splitString(const string& str) {
+    istringstream iss(str);
+    vector<string> words;
+    string word;
+    
+    while (iss >> word) {
+        words.push_back(word);
+    }
+    
+    return words;
+}
+
+/* 
+ *  Converts a line of vm code into a line of assembly code
+ */
+static string vm2Asm(const vector<string> vmLine)
+{
+    if(arithmeticMap.find(vmLine[0]) != arithmeticMap.end())
+    {
+        return arithmeticMap.at(vmLine[0])();
+    }
+    else if(memAccessMap.find(vmLine[0]) != memAccessMap.end())
+    {
+        return memAccessMap.at(vmLine[0])(vmLine[1], stoi(vmLine[2]));
+    }
+    else
+    {
+        cerr << "Invalid vm operand " << vmLine[0] << endl;
+        return "";
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    // Assert arg count
+    if (argc != 2) 
+    {
+        cerr << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
+
+    // Open the asm file
+    string fileArg(argv[1]);
+    ifstream vmFile(fileArg);
+    if (!vmFile.is_open()) 
+    {
+        cerr << "Failed to open the vm file." << endl;
+        return 1;
+    }
+
+    // Open the hack file
+    ofstream asmFile(format("{}.asm", fileArg.substr(0, fileArg.find('.'))));
+    if (!asmFile.is_open()) {
+        cerr << "Failed to open the machine code file." << endl;
+        return 1;
+    }
+
+    // Initializations
+    string tempAsm = "// Virtual Machine code to assembly code in C++ for the Hack computer.\n//\t\tSonaurea/RoyalArdor\n";
+    tempAsm += "// Initializations\n";
+    tempAsm += "// Stack Init\n@256\nD=A\n@SP\nM=D\n"; // Stack Init
+    asmFile << tempAsm << endl;
+
+    // Parse virtual machine file line by line.
+    string line{};
+    vector<string> words;
+    while(getline(vmFile, line)) 
+    {
+        words = splitString(line);
+
+        if(line != "" && line[0] != '/' && line[0] != '*' && line[0] != '(')
+        {
+            asmFile << vm2Asm(words) << endl;
+        }
+    }
+    
+    // End of Program
+    tempAsm = "// End of program\n";
+    tempAsm += "(END)\n@END\n0;JMP"; // End loop
+    asmFile << tempAsm;
+
+    asmFile.close();
+    vmFile.close();
+    return 0;
+}
